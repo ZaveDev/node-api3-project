3f545d9f411e3f74f626e74900510726fb1c3479
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-09-03 13:09:41 -0400

    MVP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server")
 
-server.listen( process.env.PORT || 8000, () => {
+server.listen( process.env.PORT || 5000, () => {
   console.log(">> [port 8000] I'm listening...");
 })
